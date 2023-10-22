e8e86773e881ee92157f0e637b2523773e0b20a4
Curiosit <mailing.bot@gmail.com>
2023-10-22 22:56:13 +0200

    7_1

---
+++ Work/7_1/sample.py
@@ -0,0 +1,12 @@
+
+
+
+from validate import Integer, validated
+
+@validated
+def add(x: Integer, y:Integer) -> Integer:
+    return x + y
+
+@validated
+def pow(x: Integer, y:Integer) -> Integer:
+    return x ** y
---
+++ Work/7_1/validate.py
@@ -0,0 +1,111 @@
+# validate.py
+class Validator:
+    @classmethod
+    def check(cls, value):
+        return value
+    
+
+class Typed(Validator):
+    expected_type = object
+    @classmethod
+    def check(cls, value):
+        if not isinstance(value, cls.expected_type):
+            raise TypeError(f'Expected {cls.expected_type}')
+        return super().check(value)
+
+class Integer(Typed):
+    expected_type = int
+
+class Float(Typed):
+    expected_type = float
+
+class String(Typed):
+    expected_type = str
+
+
+
+class Positive(Validator):
+    @classmethod
+    def check(cls, value):
+        if value < 0:
+            raise ValueError('Expected >= 0')
+        return super().check(value)
+
+class NonEmpty(Validator):
+    @classmethod
+    def check(cls, value):
+        if len(value) == 0:
+            raise ValueError('Must be non-empty')
+        return super().check(value)
+    
+
+class PositiveInteger(Integer, Positive):
+    pass
+
+class PositiveFloat(Float, Positive):
+    pass
+
+class NonEmptyString(String, NonEmpty):
+    pass
+
+
+from inspect import signature
+
+class ValidatedFunction:
+    def __init__(self, func):
+        self.func = func
+        self.signature = signature(func)
+        self.annotations = dict(func.__annotations__)
+        self.retcheck = self.annotations.pop('return', None)
+
+    def __call__(self, *args, **kwargs):
+        bound = self.signature.bind(*args, **kwargs)
+        for name, val in self.annotations.items():
+            val.check(bound.arguments[name])
+        
+        result = self.func(*args, **kwargs)
+
+        if self.retcheck:
+            self.retcheck.check(result)
+
+        return result
+
+
+def validated(func):
+    
+        
+    sig = signature(func)
+    annotations = dict(func.__annotations__)
+    retcheck = annotations.pop('return', None)
+    
+    def wrapper(*args, **kwargs):
+        errors =[]
+        bound = sig.bind(*args, **kwargs)
+        # Enforce argument checks
+        for name, validator in annotations.items():
+            try:
+                validator.check(bound.arguments[name])
+            except Exception as e:
+                errors.append(f'    {name}: {e}')
+
+        if errors:
+            raise TypeError('Bad Arguments\n' + '\n'.join(errors))
+
+        result = func(*args, **kwargs)
+        # Enforce return check (if any)
+        if retcheck:
+            try:
+                retcheck.check(result)
+            except Exception as e:
+                raise TypeError(f'Bad return: {e}') from None
+        return result
+
+    return wrapper
+
+
+""" # Examples
+if __name__ == '__main__':
+    def add(x:Integer, y:Integer) -> Integer:
+        return x + y
+
+    add = ValidatedFunction(add) """
